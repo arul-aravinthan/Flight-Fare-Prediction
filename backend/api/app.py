33bfe84de4c1ea0f664cdbcbfbdffd19f0a7c299
Arul Aravinthan <arul@Aruls-MacBook-Air.local>
2023-05-28 00:53:33 -0700

    Testing get request to backend

--- backend/api/app.py
+++ backend/api/app.py
@@ -13,9 +13,9 @@ api = Api(app)
 CORS(app) 
 
 #Load pickled model and label encoders
-# model = pickle.load(open('model.pkl', 'rb'))
-# column_order = pickle.load(open('column_order.pkl', 'rb'))
-# ct = pickle.load(open('ct.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+column_order = pickle.load(open('column_order.pkl', 'rb'))
+ct = pickle.load(open('ct.pkl', 'rb'))
 
 @app.route("/", defaults={'path':''})
 @app.route("/<path:path>", methods = ['GET', 'POST'])
