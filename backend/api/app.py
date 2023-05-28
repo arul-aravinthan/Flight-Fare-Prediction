62ee972c75e779fe374bca77bf0407cf7c82323b
Arul Aravinthan <arul@Aruls-MacBook-Air.local>
2023-05-28 00:50:33 -0700

    Testing get request to backend

--- backend/api/app.py
+++ backend/api/app.py
@@ -8,37 +8,40 @@ from flask_cors import CORS
 import json
 
 
-app = Flask(__name__, static_url_path='', static_folder='../../frontend/build')
+app = Flask(__name__)
 api = Api(app)
 CORS(app) 
 
 #Load pickled model and label encoders
-model = pickle.load(open('model.pkl', 'rb'))
-column_order = pickle.load(open('column_order.pkl', 'rb'))
-ct = pickle.load(open('ct.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
+# column_order = pickle.load(open('column_order.pkl', 'rb'))
+# ct = pickle.load(open('ct.pkl', 'rb'))
 
 @app.route("/", defaults={'path':''})
-@app.route("/<path:path>", methods = ['POST'])
+@app.route("/<path:path>", methods = ['GET', 'POST'])
 def api(path):
-    data = request.get_data().decode('utf-8')
-    data = json.loads(data)
-    #Convert stops to categorical variable
-    if data['stops'] == 0:
-        data['stops'] = 'zero'
-    elif data['stops'] == 1:
-        data['stops'] = 'one'
-    else:
-        data['stops'] = 'two_or_more'
-    # Make prediction using model loaded from disk as per the data.
+    if request.method == 'GET':
+        return 'Hello world!'
+    elif request.method == 'POST':
+        data = request.get_data().decode('utf-8')
+        data = json.loads(data)
+        #Convert stops to categorical variable
+        if data['stops'] == 0:
+            data['stops'] = 'zero'
+        elif data['stops'] == 1:
+            data['stops'] = 'one'
+        else:
+            data['stops'] = 'two_or_more'
+        # Make prediction using model loaded from disk as per the data.
 
-    input_features = pd.DataFrame([], columns=column_order)
-    input_features = pd.concat([input_features, pd.DataFrame(data, index=[0])], axis=0)
+        input_features = pd.DataFrame([], columns=column_order)
+        input_features = pd.concat([input_features, pd.DataFrame(data, index=[0])], axis=0)
 
-    input_features = ct.transform(input_features)
+        input_features = ct.transform(input_features)
 
-    prediction = model.predict(input_features)
-    output = np.float64(prediction[0])
-    return jsonify({'price': output})
+        prediction = model.predict(input_features)
+        output = np.float64(prediction[0])
+        return jsonify({'price': output})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
