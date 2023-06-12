6606b958fb2474d7d936a02e5f656435e1b6ce62
Arul Aravinthan <arul@Aruls-MacBook-Air.local>
2023-06-12 13:50:57 -0700

    Changed file structure

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from flask import Flask, jsonify, request
 import dill
-from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 import json
@@ -16,11 +15,11 @@ model = dill.load(open('model.pkl', 'rb'))
 column_order = dill.load(open('column_order.pkl', 'rb'))
 ct = dill.load(open('ct.pkl', 'rb'))
 
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path':''},  methods = ['GET', 'POST'])
 @app.route("/<path:path>", methods = ['GET', 'POST'])
 def api(path):
     if request.method == 'GET':
-        return 'Hello world!'
+        return 'Welcome to the backend!'
     elif request.method == 'POST':
         data = request.get_data().decode('utf-8')
         data = json.loads(data)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,87 +1,87 @@
-import './App.css';
-import React, { useState } from 'react';
-import axios from 'axios'
-import { useForm, Controller} from "react-hook-form";
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
 import { Slider } from "@material-ui/core";
-import { ThemeProvider } from '@material-ui/core/styles';
-import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
 
 function App() {
   const [price, setPrice] = useState(0);
 
-  const {
-    register,
-    handleSubmit,
-    control,
-  } = useForm();
+  const { register, handleSubmit, control } = useForm();
 
   const onSubmit = (data) => {
-    console.log(JSON.stringify(data))
-      axios.post('http://127.0.0.1:5000/api', 
-        JSON.stringify(data),
-      ).then(response => {
-        setPrice(response.data['price'])
-        console.log("SUCCESS", response)}
-      ).catch(error => {  
-        console.log(error)
+    console.log(JSON.stringify(data));
+    axios
+      .post(
+        "https://flight-fare-backend-oln6m6cm6a-uc.a.run.app/api",
+        JSON.stringify(data)
+      )
+      .then((response) => {
+        setPrice(response.data["price"]);
+        console.log("SUCCESS", response);
       })
-  }; 
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const muiTheme = createTheme({
-    overrides:{
+    overrides: {
       MuiSlider: {
         root: {
-          color: '#FF7276',
+          color: "#FF7276",
           height: 8,
-          '&$vertical': {
-            width: 8
-          }
+          "&$vertical": {
+            width: 8,
+          },
         },
         thumb: {
           height: 24,
           width: 24,
-          backgroundColor: '#fff',
-          border: '2px solid currentColor',
+          backgroundColor: "#fff",
+          border: "2px solid currentColor",
           marginTop: -8,
           marginLeft: -12,
-          '&:focus, &:hover': {
-            boxShadow: '0px 0px 0px 8px rgba(84, 199, 97, 0.16)'
+          "&:focus, &:hover": {
+            boxShadow: "0px 0px 0px 8px rgba(84, 199, 97, 0.16)",
           },
-          '&$active': {
-            boxShadow: '0px 0px 0px 12px rgba(84, 199, 97, 0.16)'
-          }
+          "&$active": {
+            boxShadow: "0px 0px 0px 12px rgba(84, 199, 97, 0.16)",
+          },
         },
         active: {},
         valueLabel: {
-          left: 'calc(-50% + 4px)'
+          left: "calc(-50% + 4px)",
         },
         track: {
           height: 8,
-          borderRadius: 4
+          borderRadius: 4,
         },
         rail: {
           height: 8,
-          borderRadius: 4
+          borderRadius: 4,
         },
         vertical: {
-          '& $rail': {
-            width: 8
+          "& $rail": {
+            width: 8,
           },
-          '& $track': {
-            width: 8
+          "& $track": {
+            width: 8,
           },
-          '& $thumb': {
+          "& $thumb": {
             marginLeft: -8,
-            marginBottom: -11
-          }
-        }
-      }
-      }
-    });
+            marginBottom: -11,
+          },
+        },
+      },
+    },
+  });
 
   return (
-    <div className="App" >
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Airline</label>
         <select {...register("airline", { required: true })}>
           <option value="SpiceJet">SpiceJet</option>
@@ -99,8 +99,8 @@ function App() {
           <option value="Bangalore">Bangalore</option>
           <option value="Kolkata">Kolkata</option>
           <option value="Hyderabad">Hyderabad</option>
-        </select>    
-        
+        </select>
+
         <label>Departure Time</label>
         <select {...register("departure_time", { required: true })}>
           <option value="Early_Morning">Early Morning</option>
@@ -108,7 +108,7 @@ function App() {
           <option value="Afternoon">Afternoon</option>
           <option value="Evening">Evening</option>
           <option value="Night">Night</option>
-        </select>            
+        </select>
 
         <label>Number of Stops</label>
         <Controller
@@ -117,16 +117,16 @@ function App() {
           defaultValue={0}
           render={({ field }) => (
             <ThemeProvider theme={muiTheme}>
-            <Slider
-              {...field}
-              onChange={(_, value) => {
-                field.onChange(value);
-              }}
-              valueLabelDisplay="auto"
-              marks
-              max={5}
-              step={1}
-            />
+              <Slider
+                {...field}
+                onChange={(_, value) => {
+                  field.onChange(value);
+                }}
+                valueLabelDisplay="auto"
+                marks
+                max={5}
+                step={1}
+              />
             </ThemeProvider>
           )}
         />
@@ -138,7 +138,7 @@ function App() {
           <option value="Afternoon">Afternoon</option>
           <option value="Evening">Evening</option>
           <option value="Night">Night</option>
-        </select>    
+        </select>
 
         <label>Destination City</label>
         <select {...register("destination_city", { required: true })}>
@@ -147,7 +147,7 @@ function App() {
           <option value="Bangalore">Bangalore</option>
           <option value="Kolkata">Kolkata</option>
           <option value="Hyderabad">Hyderabad</option>
-        </select>  
+        </select>
 
         <label>Ticket Class</label>
         <select {...register("class", { required: true })}>
@@ -162,19 +162,19 @@ function App() {
           defaultValue={1}
           render={({ field }) => (
             <ThemeProvider theme={muiTheme}>
-            <Slider
-              {...field}
-              onChange={(_, value) => {
-                field.onChange(value);
-              }}
-              valueLabelDisplay="auto"
-              min={1}
-              step={0.01}
-              max={50}
-            />
+              <Slider
+                {...field}
+                onChange={(_, value) => {
+                  field.onChange(value);
+                }}
+                valueLabelDisplay="auto"
+                min={1}
+                step={0.01}
+                max={50}
+              />
             </ThemeProvider>
           )}
-        />       
+        />
 
         <label>How many days are left until your flight?</label>
         <Controller
@@ -183,24 +183,26 @@ function App() {
           defaultValue={1}
           render={({ field }) => (
             <ThemeProvider theme={muiTheme}>
-            <Slider
-              {...field}
-              onChange={(_, value) => {
-                field.onChange(value);
-              }}
-              valueLabelDisplay="auto"
-              min={1}
-              step={1}
-              max={50}
-            />
+              <Slider
+                {...field}
+                onChange={(_, value) => {
+                  field.onChange(value);
+                }}
+                valueLabelDisplay="auto"
+                min={1}
+                step={1}
+                max={50}
+              />
             </ThemeProvider>
           )}
-        />              
-      <input type="submit" />
-    </form>
-    <div>
-      <h1>Price: ₹{price.toFixed(2)}, or ${(price * 0.012).toFixed(2)} </h1>
-    </div>
+        />
+        <input type="submit" />
+      </form>
+      <div>
+        <h1>
+          Price: ₹{price.toFixed(2)}, or ${(price * 0.012).toFixed(2)}{" "}
+        </h1>
+      </div>
     </div>
   );
 }
